b2decc30bd34c2b2fcf015d5bbd3a276104bc6d0
Dennis-bp575 <dennis.ubaldo@gmail.com>
2026-07-16 03:40:01 -0300

    Create months.ts

---
+++ data/months.ts
@@ -0,0 +1,298 @@
+// Contém o número do ÚLTIMO mês de cada ano (Primavera 1 até 19)
+export const primavera235: number[] = [
+  12,  // Primavera 1 termina no mês 12
+  24,  // Primavera 2 termina no mês 24
+  37,  // Primavera 3 termina no mês 37 (inclui O Flutuar)
+  49,  // Primavera 4 termina no mês 49
+  61,  // Primavera 5 termina no mês 61
+  74,  // Primavera 6 termina no mês 74 (inclui O Flutuar)
+  86,  // Primavera 7 termina no mês 86
+  99,  // Primavera 8 termina no mês 99 (inclui O Flutuar)
+  111, // Primavera 9 termina no mês 111
+  123, // Primavera 10 termina no mês 123
+  136, // Primavera 11 termina no mês 136 (inclui O Flutuar)
+  148, // Primavera 12 termina no mês 148
+  160, // Primavera 13 termina no mês 160
+  173, // Primavera 14 termina no mês 173 (inclui O Flutuar)
+  185, // Primavera 15 termina no mês 185
+  197, // Primavera 16 termina no mês 197
+  210, // Primavera 17 termina no mês 210 (inclui O Flutuar)
+  222, // Primavera 18 termina no mês 222
+  235  // Primavera 19 termina no mês 235 (inclui O Flutuar)
+];
+
+
+export const poeticMonths = [
+  // --- Primavera 1 (1 a 12) ---
+  'O Alvorecer',   // Mês 1
+  'O Florescer',   // Mês 2
+  'O Esverdear',   // Mês 3
+  'O Irradiar',    // Mês 4
+  'O Amadurecer',  // Mês 5
+  'O Acolher',     // Mês 6
+  'O Entardecer',  // Mês 7
+  'O Silenciar',   // Mês 8
+  'O Despir',      // Mês 9
+  'O Recolher',    // Mês 10
+  'O Adormecer',   // Mês 11
+  'O Gestar',      // Mês 12
+
+  // --- Primavera 2 (13 a 24) ---
+  'O Alvorecer',   // Mês 13
+  'O Florescer',   // Mês 14
+  'O Esverdear',   // Mês 15
+  'O Irradiar',    // Mês 16
+  'O Amadurecer',  // Mês 17
+  'O Acolher',     // Mês 18
+  'O Entardecer',  // Mês 19
+  'O Silenciar',   // Mês 20
+  'O Despir',      // Mês 21
+  'O Recolher',    // Mês 22
+  'O Adormecer',   // Mês 23
+  'O Gestar',      // Mês 24
+
+  // --- Primavera 3 (25 a 37) ---
+  'O Alvorecer',   // Mês 25
+  'O Florescer',   // Mês 26
+  'O Esverdear',   // Mês 27
+  'O Irradiar',    // Mês 28
+  'O Amadurecer',  // Mês 29
+  'O Acolher',     // Mês 30
+  'O Entardecer',  // Mês 31
+  'O Silenciar',   // Mês 32
+  'O Despir',      // Mês 33
+  'O Recolher',    // Mês 34
+  'O Adormecer',   // Mês 35
+  'O Gestar',      // Mês 36
+  'O Flutuar',     // Mês 37
+
+  // --- Primavera 4 (38 a 49) ---
+  'O Alvorecer',   // Mês 38
+  'O Florescer',   // Mês 39
+  'O Esverdear',   // Mês 40
+  'O Irradiar',    // Mês 41
+  'O Amadurecer',  // Mês 42
+  'O Acolher',     // Mês 43
+  'O Entardecer',  // Mês 44
+  'O Silenciar',   // Mês 45
+  'O Despir',      // Mês 46
+  'O Recolher',    // Mês 47
+  'O Adormecer',   // Mês 48
+  'O Gestar',      // Mês 49
+
+  // --- Primavera 5 (50 a 61) ---
+  'O Alvorecer',   // Mês 50
+  'O Florescer',   // Mês 51
+  'O Esverdear',   // Mês 52
+  'O Irradiar',    // Mês 53
+  'O Amadurecer',  // Mês 54
+  'O Acolher',     // Mês 55
+  'O Entardecer',  // Mês 56
+  'O Silenciar',   // Mês 57
+  'O Despir',      // Mês 58
+  'O Recolher',    // Mês 59
+  'O Adormecer',   // Mês 60
+  'O Gestar',      // Mês 61
+
+  // --- Primavera 6 (62 a 74) ---
+  'O Alvorecer',   // Mês 62
+  'O Florescer',   // Mês 63
+  'O Esverdear',   // Mês 64
+  'O Irradiar',    // Mês 65
+  'O Amadurecer',  // Mês 66
+  'O Acolher',     // Mês 67
+  'O Entardecer',  // Mês 68
+  'O Silenciar',   // Mês 69
+  'O Despir',      // Mês 70
+  'O Recolher',    // Mês 71
+  'O Adormecer',   // Mês 72
+  'O Gestar',      // Mês 73
+  'O Flutuar',     // Mês 74
+
+  // --- Primavera 7 (75 a 86) ---
+  'O Alvorecer',   // Mês 75
+  'O Florescer',   // Mês 76
+  'O Esverdear',   // Mês 77
+  'O Irradiar',    // Mês 78
+  'O Amadurecer',  // Mês 79
+  'O Acolher',     // Mês 80
+  'O Entardecer',  // Mês 81
+  'O Silenciar',   // Mês 82
+  'O Despir',      // Mês 83
+  'O Recolher',    // Mês 84
+  'O Adormecer',   // Mês 85
+  'O Gestar',      // Mês 86
+
+  // --- Primavera 8 (87 a 99) ---
+  'O Alvorecer',   // Mês 87
+  'O Florescer',   // Mês 88
+  'O Esverdear',   // Mês 89
+  'O Irradiar',    // Mês 90
+  'O Amadurecer',  // Mês 91
+  'O Acolher',     // Mês 92
+  'O Entardecer',  // Mês 93
+  'O Silenciar',   // Mês 94
+  'O Despir',      // Mês 95
+  'O Recolher',    // Mês 96
+  'O Adormecer',   // Mês 97
+  'O Gestar',      // Mês 98
+  'O Flutuar',     // Mês 99
+
+  // --- Primavera 9 (100 a 111) ---
+  'O Alvorecer',   // Mês 100
+  'O Florescer',   // Mês 101
+  'O Esverdear',   // Mês 102
+  'O Irradiar',    // Mês 103
+  'O Amadurecer',  // Mês 104
+  'O Acolher',     // Mês 105
+  'O Entardecer',  // Mês 106
+  'O Silenciar',   // Mês 107
+  'O Despir',      // Mês 108
+  'O Recolher',    // Mês 109
+  'O Adormecer',   // Mês 110
+  'O Gestar',      // Mês 111
+
+  // --- Primavera 10 (112 a 123) ---
+  'O Alvorecer',   // Mês 112
+  'O Florescer',   // Mês 113
+  'O Esverdear',   // Mês 114
+  'O Irradiar',    // Mês 115
+  'O Amadurecer',  // Mês 116
+  'O Acolher',     // Mês 117
+  'O Entardecer',  // Mês 118
+  'O Silenciar',   // Mês 119
+  'O Despir',      // Mês 120
+  'O Recolher',    // Mês 121
+  'O Adormecer',   // Mês 122
+  'O Gestar',      // Mês 123
+
+  // --- Primavera 11 (124 a 136) ---
+  'O Alvorecer',   // Mês 124
+  'O Florescer',   // Mês 125
+  'O Esverdear',   // Mês 126
+  'O Irradiar',    // Mês 127
+  'O Amadurecer',  // Mês 128
+  'O Acolher',     // Mês 129
+  'O Entardecer',  // Mês 130
+  'O Silenciar',   // Mês 131
+  'O Despir',      // Mês 132
+  'O Recolher',    // Mês 133
+  'O Adormecer',   // Mês 134
+  'O Gestar',      // Mês 135
+  'O Flutuar',     // Mês 136
+
+  // --- Primavera 12 (137 a 148) ---
+  'O Alvorecer',   // Mês 137
+  'O Florescer',   // Mês 138
+  'O Esverdear',   // Mês 139
+  'O Irradiar',    // Mês 140
+  'O Amadurecer',  // Mês 141
+  'O Acolher',     // Mês 142
+  'O Entardecer',  // Mês 143
+  'O Silenciar',   // Mês 144
+  'O Despir',      // Mês 145
+  'O Recolher',    // Mês 146
+  'O Adormecer',   // Mês 147
+  'O Gestar',      // Mês 148
+
+  // --- Primavera 13 (149 a 160) ---
+  'O Alvorecer',   // Mês 149
+  'O Florescer',   // Mês 150
+  'O Esverdear',   // Mês 151
+  'O Irradiar',    // Mês 152
+  'O Amadurecer',  // Mês 153
+  'O Acolher',     // Mês 154
+  'O Entardecer',  // Mês 155
+  'O Silenciar',   // Mês 156
+  'O Despir',      // Mês 157
+  'O Recolher',    // Mês 158
+  'O Adormecer',   // Mês 159
+  'O Gestar',      // Mês 160
+
+  // --- Primavera 14 (161 a 173) ---
+  'O Alvorecer',   // Mês 161
+  'O Florescer',   // Mês 162
+  'O Esverdear',   // Mês 163
+  'O Irradiar',    // Mês 164
+  'O Amadurecer',  // Mês 165
+  'O Acolher',     // Mês 166
+  'O Entardecer',  // Mês 167
+  'O Silenciar',   // Mês 168
+  'O Despir',      // Mês 169
+  'O Recolher',    // Mês 170
+  'O Adormecer',   // Mês 171
+  'O Gestar',      // Mês 172
+  'O Flutuar',     // Mês 173
+
+  // --- Primavera 15 (174 a 185) ---
+  'O Alvorecer',   // Mês 174
+  'O Florescer',   // Mês 175
+  'O Esverdear',   // Mês 176
+  'O Irradiar',    // Mês 177
+  'O Amadurecer',  // Mês 178
+  'O Acolher',     // Mês 179
+  'O Entardecer',  // Mês 180
+  'O Silenciar',   // Mês 181
+  'O Despir',      // Mês 182
+  'O Recolher',    // Mês 183
+  'O Adormecer',   // Mês 184
+  'O Gestar',      // Mês 185
+
+  // --- Primavera 16 (186 a 197) ---
+  'O Alvorecer',   // Mês 186
+  'O Florescer',   // Mês 187
+  'O Esverdear',   // Mês 188
+  'O Irradiar',    // Mês 189
+  'O Amadurecer',  // Mês 190
+  'O Acolher',     // Mês 191
+  'O Entardecer',  // Mês 192
+  'O Silenciar',   // Mês 193
+  'O Despir',      // Mês 194
+  'O Recolher',    // Mês 195
+  'O Adormecer',   // Mês 196
+  'O Gestar',      // Mês 197
+
+  // --- Primavera 17 (198 a 210) ---
+  'O Alvorecer',   // Mês 198
+  'O Florescer',   // Mês 199
+  'O Esverdear',   // Mês 200
+  'O Irradiar',    // Mês 201
+  'O Amadurecer',  // Mês 202
+  'O Acolher',     // Mês 203
+  'O Entardecer',  // Mês 204
+  'O Silenciar',   // Mês 205
+  'O Despir',      // Mês 206
+  'O Recolher',    // Mês 207
+  'O Adormecer',   // Mês 208
+  'O Gestar',      // Mês 209
+  'O Flutuar',     // Mês 210
+
+  // --- Primavera 18 (211 a 222) ---
+  'O Alvorecer',   // Mês 211
+  'O Florescer',   // Mês 212
+  'O Esverdear',   // Mês 213
+  'O Irradiar',    // Mês 214
+  'O Amadurecer',  // Mês 215
+  'O Acolher',     // Mês 216
+  'O Entardecer',  // Mês 217
+  'O Silenciar',   // Mês 218
+  'O Despir',      // Mês 219
+  'O Recolher',    // Mês 220
+  'O Adormecer',   // Mês 221
+  'O Gestar',      // Mês 222
+
+  // --- Primavera 19 (223 a 235) ---
+  'O Alvorecer',   // Mês 223
+  'O Florescer',   // Mês 224
+  'O Esverdear',   // Mês 225
+  'O Irradiar',    // Mês 226
+  'O Amadurecer',  // Mês 227
+  'O Acolher',     // Mês 228
+  'O Entardecer',  // Mês 229
+  'O Silenciar',   // Mês 230
+  'O Despir',      // Mês 231
+  'O Recolher',    // Mês 232
+  'O Adormecer',   // Mês 233
+  'O Gestar',      // Mês 234
+  'O Flutuar'      // Mês 235
+];
