5885e78a68a5093856db5ed1f9a6dcf7184f0331
Dennis-bp575 <dennis.ubaldo@gmail.com>
2026-07-20 12:06:19 -0300

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ window.addEventListener('DOMContentLoaded', () => {
     card.style.borderTop = `6px solid ${corDoMes}`;
   }
 
-  if (moonDisplay) moonDisplay.innerHTML = `<img src="${dadosAtuais.moon}" alt="Lua" class="w-16 h-16 object-contain" />`
+  if (moonDisplay) moonDisplay.innerHTML = `<img src="${dadosAtuais.moon}" alt="Lua" class="w-16 h-16 object-contain mix-blend-screen" />`
   if (poeticMonth) poeticMonth.innerText = dadosAtuais.poeticMonth;
   if (lunarDay) lunarDay.innerText = `Hoje é o ${dadosAtuais.lunarDay}º dia lunar`;
   if (periodDisplay) periodDisplay.innerText = dadosAtuais.period;
